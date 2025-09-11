fa8bb58e4476ccf055c80e9f916823a9b920d9c6
MDErik <erik.mareniusdehlin@elev.ga.ntig.se>
2025-09-11 16:39:42 +0200

    klar

--- app.rb
+++ app.rb
@@ -1,1 +1,26 @@
 require 'sinatra'
+require 'slim'
+require 'sinatra/reloader'
+
+get('/') do
+  slim(:start)
+
+end
+
+get('/bye') do
+  slim(:bye)
+
+end
+
+get('/fruits') do
+  @fruits = ["Äpple","Banan","Apelsin"]
+  slim(:fruits)
+ end
+ 
+ get('/frukt/:id') do
+  fruits = ["Äpple","Banan","Apelsin"]
+  id = params[:id].to_i
+  @Fruit = fruits[id]
+  slim(:frukt)
+  
+end
